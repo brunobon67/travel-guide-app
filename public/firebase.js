d8ee2fc166351268155a1ed9e43f1c79d8e2f113
brunobon67 <83511095+brunobon67@users.noreply.github.com>
2025-03-31 22:03:04 +0200

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,6 +1,7 @@
 // public/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBTU9waiz51hZcBM3SeV23sVkZZzDPn7XA",
@@ -14,6 +15,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { app, auth };
-
+export { app, auth, db };
