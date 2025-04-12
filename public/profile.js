0f701d2dc61633053f07942b37a6250620c0dd0c
brunobon67 <83511095+brunobon67@users.noreply.github.com>
2025-04-12 11:01:08 +0200

    Update profile.js

--- public/profile.js
+++ public/profile.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBTU9waiz51hZcBM3SeV23sVkZZzDPn7XA",
   authDomain: "travel-guide-11210.firebaseapp.com",
   projectId: "travel-guide-11210",
-  storageBucket: "travel-guide-11210.firebasestorage.app",
+storageBucket: "travel-guide-11210.appspot.com"
   messagingSenderId: "578059026982",
   appId: "1:578059026982:web:f0b3082c9c2f5ea0069ce7"
 };
