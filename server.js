e007a65bfd27dd2a31beb9c83f944d9866471751
brunobon67 <83511095+brunobon67@users.noreply.github.com>
2025-04-24 14:07:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, ".")));
 
 // Routes
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index (28).html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Generate Itinerary route
